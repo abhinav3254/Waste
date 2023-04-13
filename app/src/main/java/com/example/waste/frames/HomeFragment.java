980e10ad07ff31ad51ee3448d8c4d6b45a56ce94
abhinav3254 <abhinav3254@gmail.com>
2023-04-13 10:36:37 +0530

    Adding bg img to recyclerview

--- app/src/main/java/com/example/waste/frames/HomeFragment.java
+++ app/src/main/java/com/example/waste/frames/HomeFragment.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.waste.R;
 import com.example.waste.database.Adapter;
@@ -24,12 +25,16 @@ public class HomeFragment extends Fragment {
     RecyclerView recyclerView;
     Pojo pojo;
 
+    ImageView emptyImage;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         recyclerView = view.findViewById(R.id.recycleView);
+
+        emptyImage = view.findViewById(R.id.emptyImage);
 
         Adapter adapter = new Adapter(view.getContext(),readAll(view.getContext()));
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
@@ -43,12 +48,14 @@ public class HomeFragment extends Fragment {
         List<Pojo> list = new ArrayList<>();
         Cursor cursor = dbHelper.readAll();
         if (cursor.getCount() == 0) {
-
+            emptyImage.setVisibility(View.VISIBLE);
         } else {
             while (cursor.moveToNext()) {
                 pojo = new Pojo(cursor.getString(0),cursor.getString(1),cursor.getString(4),cursor.getString(3),cursor.getString(2));
                 list.add(pojo);
             }
+            emptyImage.setVisibility(View.INVISIBLE);
+
         }
         return list;
     }
